a6f1888d22065aab0f711975e442cd83d66e696c
dragon_li <49151509+1097452462@users.noreply.github.com>
2022-02-14 15:19:42 +0800

    Update gift.setting.js

--- src/gift.setting.js
+++ src/gift.setting.js
@@ -23,7 +23,7 @@ export const homeConfig = {
   // 抽奖转盘的动画时间，单位毫秒
   timeout: 5000,
   // 指定赠送的礼物的索引（第几个礼物中奖），取值范围 0-8
-  targetGiftIndex: null,
+  targetGiftIndex: 5,
 };
 
 // 礼物清单
@@ -31,12 +31,12 @@ export const homeConfig = {
 // 图片放在 public/images 中，建议是正方形，否则影响视觉体验
 // name 是礼物的全称；alias 是礼物的简称（用来显示在抽奖方格里）
 export const gifts = [
-  { key: 'q', name: '520红包', alias: 'Dior双飞', image: '/images/1.png',  description: '「520块人民币！！」' },
+  { key: 'q', name: '520红包', alias: '520', image: '/images/1.png',  description: '「520块人民币！！」' },
   { key: 'w', name: '腾讯视频年VIP', alias: 'VIP', image: '/images/2.png',  description: '“为你承包VIP”' },
-  { key: 'e', name: '999红包', alias: '小黑瓶套装', image: '/images/3.png',  description: '「999块人民币！！！」' },
-  { key: 'r', name: '199红包', alias: '520', image: '/images/6.png',  description: '199块人民币！' },
-  { key: 't', name: '299红包', alias: 'Dior星空', image: '/images/5.png',  description: '299块人民币！' },
+  { key: 'e', name: '999红包', alias: '999', image: '/images/3.png',  description: '「999块人民币！！！」' },
+  { key: 'r', name: '199红包', alias: '199', image: '/images/6.png',  description: '199块人民币！' },
+  { key: 't', name: '299红包', alias: '299', image: '/images/5.png',  description: '299块人民币！' },
   { key: 'y', name: '1314红包', alias: '1314', image: '/images/6.png',  description: '“从今往后，我都会在你旁边”' },
-  { key: 'u', name: '99', alias: 'UR', image: '/images/7.png',  description: '小小心意~' },
-  { key: 'i', name: '谢谢惠顾！嘿嘿', alias: '戴森', image: '/images/8.png',  description: '5555555555' },
+  { key: 'u', name: '99', alias: '99', image: '/images/7.png',  description: '小小心意~' },
+  { key: 'i', name: '谢谢惠顾！嘿嘿', alias: '谢谢惠顾！', image: '/images/8.png',  description: '5555555555' },
 ];
